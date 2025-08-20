58275414aee5a6fb72df539ad09d33f41bbc4c1d
Rafael-Leivas <rafaelblsteam@gmail.com>
2025-08-20 20:07:56 -0300

    Start project

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,16 @@
+import ExpenseTable from "./components/TransactionTable";
+
+const Home = () => {
+  return (
+    <>
+      <div className="">
+
+        <div className="max-w-7xl mx-auto pt-20">
+          <ExpenseTable />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
