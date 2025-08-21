2b773e0fbdc452ff6f792f2a47541d3b1522dc28
Rafael-Leivas <rafaelblsteam@gmail.com>
2025-08-20 23:01:37 -0300

    Add modal component of shadcn and import AddNewItem component

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,11 +1,35 @@
+import { Button } from "@/Components/ui/button";
+import { Dialog, DialogTrigger } from "@/Components/ui/dialog";
+import { useState } from "react";
 import TransactionTable from "./components/Table/TransactionTable";
+import AddNewItem from "./components/AddNewItem/AddNewItem";
 
 const Home = () => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const openAddTransactionModal = () => {
+    console.log("Open modal to add transaction");
+    setIsDialogOpen(true);
+  };
+
   return (
     <>
       <div className="">
-
         <div className="max-w-7xl mx-auto pt-20">
+          <div className="flex justify-end">
+            <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+              <DialogTrigger asChild>
+                <Button
+                  onClick={openAddTransactionModal}
+                  variant="link"
+                  className="cursor-pointer text-accent mb-2"
+                >
+                  + Adicionar nova transação
+                </Button>
+              </DialogTrigger>
+              <AddNewItem />
+            </Dialog>
+          </div>
           <TransactionTable />
         </div>
       </div>
